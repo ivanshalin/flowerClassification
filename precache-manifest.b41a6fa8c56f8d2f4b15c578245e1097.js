6b9e4cba9a58371eda9f954b98f6764e8bcaa92b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-21 14:37:04 +0000

    Deploy shubhank-saxena/flowerClassification to github.com/shubhank-saxena/flowerClassification.git:gh-pages

---
+++ precache-manifest.b41a6fa8c56f8d2f4b15c578245e1097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5334a980f47aa95bf3910247382ef2ad",
+    "url": "/flowerClassification/index.html"
+  },
+  {
+    "revision": "98d00855b179b2e2d4b2",
+    "url": "/flowerClassification/static/css/main.c3ec641c.chunk.css"
+  },
+  {
+    "revision": "a4f63cf73f74cb760d96",
+    "url": "/flowerClassification/static/js/2.145adbd5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/flowerClassification/static/js/2.145adbd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98d00855b179b2e2d4b2",
+    "url": "/flowerClassification/static/js/main.7173379e.chunk.js"
+  },
+  {
+    "revision": "0c76a62648f6d65b35c3",
+    "url": "/flowerClassification/static/js/runtime-main.58325798.js"
+  }
+]);
